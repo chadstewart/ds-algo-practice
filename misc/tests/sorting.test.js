1fafeb163acefb42bfff7a87fb35a6e1e9b14260
Chad Stewart <chadrhonanstewart@gmail.com>
2020-08-09 11:12:14 -0500

    Added more tests for merge sort. Will explore trying to come up with more test cases for the different merge sort units before moving on to quick sort.

--- misc/tests/sorting.test.js
+++ misc/tests/sorting.test.js
@@ -1,27 +1,75 @@
 const { quickSortTesting, mergeSortTesting } = require('../../utils/current-test-config');
+const { sorted } = require('../../utils/test-utilities');
+
+let testArray, sortedArray = [];
 
 describe('Merge Sort Testing', () => {
-    const testArray = [5,6,1,8,3];
 
-
-    test('Should return null if no value', () => {
-        expect(mergeSortTesting.mergeSort()).toBeNull();
+    beforeEach(() => {
+        testArray = [5,8,4,9,3,18,2,19];
+        sortedArray = [1,2,3,4,5,6];
     });
 
-    test('Should sort array properly when given value', () => {
-        expect(mergeSortTesting.mergeSort(testArray)).toEqual([1,3,5,6,8]);
+    describe('Merge Sort Function', () => {
+
+        test('Should return null if no value', () => {
+            expect(mergeSortTesting.mergeSort()).toBeNull();
+        });
+
+        test('Should sort array properly when given value', () => {
+            expect(sorted(mergeSortTesting.mergeSort(testArray))).toBeTruthy();
+        });
+
+        test('If given a sorted array, should reproduce the array', () => {
+            expect(mergeSortTesting.mergeSort(sortedArray)).toEqual([1,2,3,4,5,6]);
+        });
+
     });
+
+    describe('Merge Function', () => {
+
+        test('Given two sorted arrays, merge should return a sorted array', () => {
+            expect(sorted(mergeSortTesting.merge([1,5,8], [2,7]))).toBeTruthy();
+        });
+
+    });
+
 });
 
 describe('Quick Sort Testing', () => {
-    const testArray = [5,6,1,8,3];
 
-
-    test('Should return null if no value', () => {
-        expect(quickSortTesting.quickSort()).toBeNull();
+    beforeEach(() => {
+        testArray = [5,8,4,9,3,18,2,19];
+        sortedArray = [1,2,3,4,5,6];
     });
 
-    test('Should sort array properly when given value', () => {
-        expect(quickSortTesting.quickSort(testArray)).toEqual([1,3,5,6,8]);
+
+    describe('Quick Sort Function', () => {
+
+        test('Should return null if no value', () => {
+            expect(quickSortTesting.quickSort()).toBeNull();
+        });
+
+        test('Should sort array properly when given value', () => {
+            expect(sorted(quickSortTesting.quickSort(testArray))).toBeTruthy();
+        });
+
+        test('If given a sorted array, should reproduce the array', () => {
+            expect(quickSortTesting.quickSort(sortedArray)).toEqual([1,2,3,4,5,6]);
+        });
+
     });
+
+    describe('Random Partition Function', () => {
+
+    });
+
+    describe('Partition Function', () => {
+
+    });
+
+    describe('Swap Function', () => {
+
+    });
+
 });
---
+++ utils/test-utilities.js
@@ -0,0 +1,11 @@
+function sorted(array) {
+    for(let i = 1; i <= array.length; i++) {
+        if(array[i] < array[i - 1]) return false;
+    }
+
+    return true;
+};
+
+module.exports = {
+    sorted,
+}
